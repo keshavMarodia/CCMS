53fb4f6ac69a3c0615e15673d71e0154f681e5d3
vishesh-gupta007 <f20200352@goa.bits-pilani.ac.in>
2022-06-22 19:52:37 +0530

    login page modified

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,8 +1,12 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./Navbar.css";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  const handleClick = (e) => {
+    navigate("/");
+  };
   return (
     <>
       <div className="nav-parent">
@@ -17,75 +21,77 @@ const Navbar = () => {
           <div className="officer">loremipsumlorem</div>
         </div>
         <div className="tab-container">
-          <nav>
-            <ul className="nav-options">
-              <li className="tab-items">
-                <Link
-                  to={{
-                    pathname: "/home",
-                  }}
-                  className="tab-link"
-                >
-                  Home
-                </Link>
-              </li>
-              <li className="tab-items">
-                <Link
-                  to={{
-                    pathname: "/home",
-                  }}
-                  className="tab-link"
-                >
-                  Courts
-                </Link>
-                <ul className="subnav">
-                  <li className="sub-items">
-                    <Link
-                      to={{
-                        pathname: "/home",
-                      }}
-                      className="sub-link"
-                    >
-                      City-Civil-Court
-                    </Link>
-                  </li>
-                  <li className="sub-items">
-                    <Link
-                      to={{
-                        pathname: "/home",
-                      }}
-                      className="sub-link"
-                    >
-                      LG-Court
-                    </Link>
-                  </li>
-                  <li className="sub-items">
-                    <Link
-                      to={{
-                        pathname: "/home",
-                      }}
-                      className="sub-link"
-                    >
-                      High-Court
-                    </Link>
-                  </li>
-                </ul>
-              </li>
-              <li className="tab-items">
-                <Link
-                  to={{
-                    pathname: "/home",
-                  }}
-                  className="tab-link"
-                >
-                  Reports
-                </Link>
-              </li>
-            </ul>
-          </nav>
+          {/* <nav> */}
+          <ul className="nav-options">
+            <li className="tab-items">
+              <Link
+                to={{
+                  pathname: "/home",
+                }}
+                className="tab-link"
+              >
+                Home
+              </Link>
+            </li>
+            <li className="tab-items">
+              <Link
+                to={{
+                  pathname: "/home",
+                }}
+                className="tab-link"
+              >
+                Courts
+              </Link>
+              <ul className="subnav">
+                <li className="sub-items">
+                  <Link
+                    to={{
+                      pathname: "/home",
+                    }}
+                    className="sub-link"
+                  >
+                    City-Civil-Court
+                  </Link>
+                </li>
+                <li className="sub-items">
+                  <Link
+                    to={{
+                      pathname: "/home",
+                    }}
+                    className="sub-link"
+                  >
+                    LG-Court
+                  </Link>
+                </li>
+                <li className="sub-items">
+                  <Link
+                    to={{
+                      pathname: "/home",
+                    }}
+                    className="sub-link"
+                  >
+                    High-Court
+                  </Link>
+                </li>
+              </ul>
+            </li>
+            <li className="tab-items">
+              <Link
+                to={{
+                  pathname: "/home",
+                }}
+                className="tab-link"
+              >
+                Reports
+              </Link>
+            </li>
+          </ul>
+          {/* </nav> */}
         </div>
         <div className="logout-contianer">
-          <button className="logout">Logout</button>
+          <button className="logout" onClick={handleClick}>
+            Logout
+          </button>
         </div>
       </div>
     </>
